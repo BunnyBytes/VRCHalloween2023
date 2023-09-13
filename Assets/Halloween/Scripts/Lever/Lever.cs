68d7905d428a50869b420a3f3893e807697af0a9
BunnyBytes <o.crichton96@gmail.com>
2023-09-13 19:36:03 +0100

    Added very basic lever logic

---
+++ Assets/Halloween/Scripts/Lever/Lever.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Lever : UdonSharpBehaviour
+{
+    // The lever handle that the player will pick up and bring to this object to trigger the event
+    [SerializeField] GameObject leverHandlePickup;
+    // The lever handle which will be enabled to show the puzzle has been solved
+    [SerializeField] GameObject leverHandleLocal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null)
+        {
+            Debug.Log($"{gameObject.name} colliding with: {other.name}");
+            if (leverHandlePickup != null && leverHandlePickup == other.gameObject)
+            {
+                Debug.Log("Colliding with the handle!");
+                leverHandlePickup.SetActive(false);
+                leverHandleLocal.SetActive(true);
+
+                // Start animation
+            }
+        }
+    }
+}
