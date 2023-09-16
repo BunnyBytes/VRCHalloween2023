d43683460c29740cc69412653a1561f6ca10cccc
BunnyBytes <o.crichton96@gmail.com>
2023-09-16 16:56:18 +0100

    Added basic player following

--- Assets/Halloween/Snail.cs
+++ Assets/Halloween/Snail.cs
@@ -1,13 +1,18 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.AI;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class Snail : UdonSharpBehaviour
 {
-    void Start()
+    [SerializeField] NavMeshAgent agent;
+
+    private void Update()
     {
-        
+        VRCPlayerApi target = Networking.LocalPlayer;
+
+        agent.SetDestination(target.GetPosition());
     }
 }
