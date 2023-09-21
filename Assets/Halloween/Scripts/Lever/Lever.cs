381294f371ab0becd5ddc570bc30075b7898bf8c
BunnyBytes <o.crichton96@gmail.com>
2023-09-21 18:57:40 +0100

    Added enabling and disabling of lever based on stage of events

--- Assets/Halloween/Scripts/Lever/Lever.cs
+++ Assets/Halloween/Scripts/Lever/Lever.cs
@@ -3,16 +3,23 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
 
 public class Lever : UdonSharpBehaviour
 {
     [SerializeField] GameStateManager GameStateManager;
     [SerializeField] GameObject LeverRespawn;
+    [SerializeField] GameObject gate;
 
     // The lever handle that the player will pick up and bring to this object to trigger the event
     [SerializeField] GameObject leverHandlePickup;
     // The lever handle which will be enabled to show the puzzle has been solved
     [SerializeField] GameObject leverHandleLocal;
+
+    private void Start()
+    {
+        DisableInteractive = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -29,7 +36,24 @@ public class Lever : UdonSharpBehaviour
                 
                 // Change spawn point
                 GameStateManager.SpawnPoint = LeverRespawn.transform.position;
+                SendCustomNetworkEvent(NetworkEventTarget.All, "SetEnableInteractive");
             }
         }
+    }
+
+    public override void Interact()
+    {
+        SendCustomNetworkEvent(NetworkEventTarget.All, "DisableGate");
+    }
+
+    public void SetEnableInteractive()
+    {
+        DisableInteractive = false;
+    }
+
+    public void DisableGate()
+    {
+        DisableInteractive = true;
+        gate.SetActive(false);
     }
 }
